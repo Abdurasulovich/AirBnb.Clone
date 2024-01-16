f129e222226a798521b90b364db3831fd2d43098
Abdurasulovich <javaengeineer@gmail.com>
2024-01-16 20:57:12 +0500

    Identity Added

---
+++ AirbnbServerApp/src/Airbnb.Infrastructure/Identity/Services/PasswordHasherService.cs
@@ -0,0 +1,17 @@
+﻿using Airbnb.Application.Common.Identity.Services.Interfaces;
+using BC=BCrypt.Net.BCrypt;
+
+namespace Airbnb.Infrastructure.Identity.Services;
+
+public class PasswordHasherService : IPasswordHasherService
+{
+    public string HashPassword(string password)
+    {
+        return BC.HashPassword(password);
+    }
+
+    public bool ValidatePassword(string password, string hashedPassword)
+    {
+        return BC.Verify(password, hashedPassword);
+    }
+}
